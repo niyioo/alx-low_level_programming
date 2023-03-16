c9de5c063785b8aca854a5483ad15eb42a6cbdc4
niyioo <niyiolagbegi@gmail.com>
2023-03-16 05:52:35 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * printf will print the line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return(1);
+}
