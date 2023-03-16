41254a3b025f67c175f819bab28ab7e50bfd7add
niyioo <niyiolagbegi@gmail.com>
2023-03-16 06:16:38 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,16 @@
 #define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 
 /**
+ * main - Entry point
  * include the library for write() function
  * #define defined the message to be printed
- * main - Entry point
- *
  *
  * Use the write() function to print the message to standard error file descriptor
- * Return 1 to show the program has error Always
+ * Return 1 to show the program has error
+ * Return: Always 0 (Success)
  */
-int main(void)
+int main (void)
 {
-	write(STDERR_FILENO, MESSAGE, sizeof(MESSAGE)-1);
+	write (STDERR_FILENO, MESSAGE, sizeof(MESSAGE)-1);
 	return(1);
 }
