12fea99de5bbbc0065bc9dcbed29757e868a4b63
niyioo <niyiolagbegi@gmail.com>
2023-03-16 06:03:04 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
-#include <stdio>
+#include <unistd.h>
 
 /**
+ *include the library for write() function
  * main - Entry point
  *
- * printf will print the line
  *
- * Return: Always 0 (Success)
+ * Use the write() function to print the message to standard error file descriptor
+ * Return 1 to show the program has error Always
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(STDERR_FILENO, MESSAGE, sizeof(MESSAGE)-1);
 	return(1);
 }
