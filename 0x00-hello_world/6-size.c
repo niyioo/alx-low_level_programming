dde052b06b9b4805d71a4d06a7e665f87d79da7a
niyioo <niyiolagbegi@gmail.com>
2023-03-16 05:33:40 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,6 +21,6 @@ int main(void)
 	printf("\nSize of an int: %zu byte(s)", sizeof(b));
 	printf("\nSize of a long int: %zu byte(s)", sizeof(c));
 	printf("\nSize of a long long int: %zu byte(s)", sizeof(d));
-	printf("\nSize of a float: %zu byte(s)", sizeof(e));
+	printf("\nSize of a float: %zu byte(s)\n", sizeof(e));
 	return (0);
 }
