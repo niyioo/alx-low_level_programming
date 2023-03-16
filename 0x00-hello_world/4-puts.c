090c6d1125b5e0982d4d4da73b4317b7e4d84664
niyioo <niyiolagbegi@gmail.com>
2023-03-16 04:01:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
-	puts("Programming is like building a multilingual puzzle");
+int main(void) {
+	puts(""Programming is like building a multilingual puzzle\n");
 	
 	return 0;
 }
