2760c1ec178f050ee93cb93bc3ddaff77318fd61
niyioo <niyiolagbegi@gmail.com>
2023-03-17 10:31:09 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,23 +9,19 @@
 
 int main(void)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; i < 9; i++)
-	{
-		for (j = i + 1; j < 10; j++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar('');
-		putchar(j + '0');
-		if (i != 8 || j != 9)
+		if (num != 9)
 		{
 			putchar(',');
 			putchar('');
 		}
 	}
-	}
+
 	putchar('\n');
+
 	return (0);
 }
