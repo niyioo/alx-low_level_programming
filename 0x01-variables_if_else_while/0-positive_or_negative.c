eae9d104d9fb7950320f3d69be526f9329948cc1
niyioo <niyiolagbegi@gmail.com>
2023-03-17 08:02:10 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,28 +9,26 @@
  */
 int main(void)
 {
-    int n;
-
+	int n;
+    
     srand(time(0));
     n = rand() - RAND_MAX / 2;
-
+    
     printf("%d is ", n);
-
+    
     if (n > 0)
     {
-        printf("positive");
+	    printf("positive");
     }
     else if (n == 0)
     {
-        printf("zero");
+	    printf("zero");
     }
     else
     {
-        printf("negative");
+	    printf("negative");
     }
-
     printf("\n");
-
     return (0);
 }
 
