c10eb663e0b269f0e29087f486dcdae9042e7a6e
niyioo <niyiolagbegi@gmail.com>
2023-03-21 06:34:06 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,26 +15,24 @@ void times_table(void)
 		for (j + 0; j <= 9; j++)
 		{
 			result = i * j;
-			if (result <= 9)
+			if (j == 0)
 			{
-				if (j > 0)
+				_putchar('0');
+			}
+			else if (result > 10)
 				{
-				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				}
 				_putchar(result + '0');
 			}
 			else
 			{
-				if (j > 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 				_putchar(result / 10 + '0');
 				_putchar(result % 10 + '0');
+				}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 		}
 		_putchar('\n');
