a80f426694ceab8eba9ab9b5763f3e00feee2cce
niyioo <niyiolagbegi@gmail.com>
2023-03-21 10:25:11 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,44 +9,43 @@
 
 void print_times_table(int n)
 {
-	int i, j, result;
-	
 	if (n < 0 || n > 15)
-	{
-		return;
-	}
+	return;
+	int i, j, product;
 
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
-			result = i * j;
-
-			if (j > 0)
+			product = i * j;
+		if (j == 0)
+	{
+		_putchar('0');
+	}
+		else
 			{
+				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				if (result < 100)
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else if (product < 100)
 				{
 					_putchar(' ');
 				}
 			}
-			if (result < 10)
+			if (product >= 0 && product < 10)
 			{
-				_putchar(result + '0');
+				_putchar(product + '0');
 			}
-			else if (result < 100)
+			else if (product >= 10)
 			{
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
 			}
-			else
-			{
-				_putchar((result / 100) + '0');
-				_putchar(((result % 100) / 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-			}
+		}
 		_putchar('\n');
 	}
 }
