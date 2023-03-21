3b1b4b13751f82317f4188c003bb671389dd4f26
niyioo <niyiolagbegi@gmail.com>
2023-03-21 08:20:16 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,25 +24,29 @@ void print_times_table(int n)
 
 			if (j > 0)
 			{
-				_putcar(' ');
+				_putcar(',');
+				_putchar(' ');
+				if (result < 100)
+				{
+					_putchar(' ');
+				}
 			}
-
 			if (result < 10)
 			{
 				_putchar(result + '0');
 			}
 			else if (result < 100)
 			{
-				_putchar((result / 100) + '0');
-				_putchar(((result / 10) % 10) + '0');
+				_putchar((result / 10) + '0');
 				_putchar((result % 10) + '0');
 			}
-
-			if (j < n)
+			else
 			{
-				_putchar(',');
-			}}
-
+				_putchar((result / 100) + '0');
+				_putchar(((result % 100) / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			}
 		_putchar('\n');
 	}
 }
