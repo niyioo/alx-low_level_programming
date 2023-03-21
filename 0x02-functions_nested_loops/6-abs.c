b898197a632499161264c2029f80cfa306cbe529
niyioo <niyiolagbegi@gmail.com>
2023-03-21 05:43:03 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @int: input
+ *
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
