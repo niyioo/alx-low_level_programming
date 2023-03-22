8f5f24130eec6d4e7e539374aaa23e2b30626b2f
niyioo <niyiolagbegi@gmail.com>
2023-03-22 06:47:24 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,33 +12,33 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int days_left = 0;
-    int i;
+	int days_in_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	int days_left = 0;
+	int i;
 
-    if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
-    {
-        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-        return;
-    }
+	if (month < 1 || month > 12 || day < 1 || day > days_in_month[month])
+	{
+	printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+	return;
+	}
 
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-    {
-        days_in_month[2] = 29;
-    }
+	if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+	{
+	days_in_month[2] = 29;
+	}
 
-    for (i = month; i <= 12; i++)
-    {
-        if (i == month)
-        {
-            days_left += days_in_month[i] - day + 1;
-        }
-        else
-        {
-            days_left += days_in_month[i];
-        }
-    }
+	for (i = month; i <= 12; i++)
+	{
+	if (i == month)
+	{
+	days_left += days_in_month[i] - day + 1;
+	}
+	else
+	{
+	days_left += days_in_month[i];
+	}
+	}
 
-    printf("Day of the year: %d\n", convert_day(month, day));
-    printf("Remaining days: %d\n", days_left);
+	printf("Day of the year: %d\n", convert_day(month, day));
+	printf("Remaining days: %d\n", days_left);
 }
