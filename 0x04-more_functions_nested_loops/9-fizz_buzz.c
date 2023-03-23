3fa2e900b455a9f4be43d38527a36d01dbeda8da
niyioo <niyiolagbegi@gmail.com>
2023-03-23 05:30:23 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Fizz-Buzz test - prints the numbers from 1 to 100 followed
- * by a new line. But for multiples of three print Fizz instead
- * of the number and for the multiples of five print Buzz. For
- * numbers which are multiples of both three and five print FizzBuzz
+ * The “Fizz-Buzz test” is an interview question designed to help filter
+ * out the 99.5% of programming job candidates who can’t seem to program
+ * their way out of a wet paper bag
  *
  * Return: 0
  */
