3940aac8f6309282aaa62b4a5eb08dadef22f078
niyioo <niyiolagbegi@gmail.com>
2023-03-23 06:12:42 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -3,11 +3,6 @@
 
 /**
  * main - Prints a Fizz Buzz program
- *
- * Description: Prints numbers 1 to 100, replacing multiples of 3 with "Fizz",
- * multiples of 5 with "Buzz", and multiples of both with "FizzBuzz"
- * @i - Input
- *
  * Return: 0
  */
 
