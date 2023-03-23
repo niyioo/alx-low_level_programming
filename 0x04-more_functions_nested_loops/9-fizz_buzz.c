5994957ce6c072c1c946ee4d9a23612f0201bb80
niyioo <niyiolagbegi@gmail.com>
2023-03-23 06:05:40 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - Prints a Fizz Buzz program
  *
  * Description: Prints numbers 1 to 100, replacing multiples of 3 with "Fizz",
  * multiples of 5 with "Buzz", and multiples of both with "FizzBuzz"
