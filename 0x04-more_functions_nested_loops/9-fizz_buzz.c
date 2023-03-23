027668a6ec079e32d19d77b1d89127a7c4944ef7
niyioo <niyiolagbegi@gmail.com>
2023-03-23 05:32:00 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 
 /**
- * The “Fizz-Buzz test” is an interview question designed to help filter
- * out the 99.5% of programming job candidates who can’t seem to program
- * their way out of a wet paper bag
+ * main - Entry point
  *
  * Return: 0
  */
