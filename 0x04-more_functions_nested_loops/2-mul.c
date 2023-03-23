8af7f90588c9d2e77d005ab7b9a044f5627dadfb
niyioo <niyiolagbegi@gmail.com>
2023-03-23 03:05:30 -0700

    1$

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiples two integer
+ * @int a: first integer
+ * @int b: second integer
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return a * b;
+}
