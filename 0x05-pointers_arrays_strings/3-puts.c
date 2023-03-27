8047ff209ff832f2aa357303be17dbc02e230fbd
niyioo <niyiolagbegi@gmail.com>
2023-03-27 06:44:33 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Function that prints a string followed by a new line to stdout
+ * @str: Input
+ *
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
