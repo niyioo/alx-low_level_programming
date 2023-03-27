faab5693b2df505ce3e1e7e2904a9b59730f2ea7
niyioo <niyiolagbegi@gmail.com>
2023-03-27 06:36:12 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: Input
+ *
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+	while (*s != '\0')
+	{
+		length++;
+			s++;
+	}
+	return (length);
+}
