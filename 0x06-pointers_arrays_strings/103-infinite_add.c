7f1d98574abf98203f5f39378afbdbdf4554657f
niyioo <niyiolagbegi@gmail.com>
2023-03-29 15:38:50 -0700

    fix 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,43 +1,55 @@
 #include "main.h"
 
 /**
- * infinite_add - Adds two numbers
- * @n1: First number
- * @n2: Second number
- * @r: Buffer to store the result
- * @size_r: Size of the buffer
+ * infinite_add - adds two numbers
+ * @n1: first number as a string
+ * @n2: second number as a string
+ * @r: buffer to store the result
+ * @size_r: size of the buffer
  *
- * Return: Pointer to the result, or 0 if the result cannot be stored
+ * Return: pointer to the result, or 0 if the result cannot be stored in r
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, len1 = 0, len2 = 0, sum, carry = 0;
+	int i, j, k, l, m, sum, carry;
 
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
-	if (len1 + 2 > size_r || len2 + 2 > size_r)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
 		return (0);
-	r[size_r - 1] = '\0';
-	for (i = len1 - 1, j = len2 - 1, k = size_r - 2; i >= 0 || j >= 0; i--, j--, k--)
+	carry = 0;
+	for (k = i - 1, l = j - 1, m = 0; k >= 0 || l >= 0; k--, l--, m++)
 	{
 		sum = carry;
-		if (i >= 0)
-			sum += n1[i] - '0';
-		if (j >= 0)
-			sum += n2[j] - '0';
-		if (k < 0)
-			return (0);
+		if (k >= 0)
+			sum += n1[k] - '0';
+		if (l >= 0)
+			sum += n2[l] - '0';
 		carry = sum / 10;
-		r[k] = (sum % 10) + '0';
-	}
-	if (carry == 1)
-	{
-		if (k < 0)
+		if (m >= size_r)
 			return (0);
-		r[k] = '1';
-		return (r + k);
+		r[m] = sum % 10 + '0';
 	}
-	return (r + k + 1);
+	if (carry && m < size_r - 1)
+	{
+		r[m] = carry + '0';
+		r[m + 1] = '\0';
+	}
+	else if (!carry && m <= size_r - 1)
+	{
+		r[m] = '\0';
+	}
+	else
+	{
+		return (0);
+	}
+	for (i = 0, j = m - 1; i < j; i++, j--)
+	{
+		sum = r[i];
+		r[i] = r[j];
+		r[j] = sum;
+	}
+	return (r);
 }
