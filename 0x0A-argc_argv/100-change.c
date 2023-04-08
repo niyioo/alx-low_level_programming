b9b1d1312ec7553f7266bbed593267f262efb2ae
niyioo <niyiolagbegi@gmail.com>
2023-04-08 08:35:09 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,16 +11,18 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int cents, coins = 0;
+	int i;
+	int cents, num_coins = 0;
+	int coins[] = {25, 10, 5, 2, 1};
+	int num_coin_types = sizeof(coins) / sizeof(int);
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	cents = atoi(argv[1]);
 
 	if (cents < 0)
@@ -29,37 +31,15 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	while (cents >= 25)
+	for (i = 0; i < num_coin_types; i++)
 	{
-		cents -= 25;
-		coins++;
+		while (cents >= coins[i])
+		{
+			cents -= coins[i];
+			num_coins++;
+		}
 	}
-
-	while (cents >= 10)
-	{
-		cents -= 10;
-		coins++;
-	}
-
-	while (cents >= 5)
-	{
-		cents -= 5;
-		coins++;
-	}
-
-	while (cents >= 2)
-	{
-		cents -= 2;
-		coins++;
-	}
-
-	while (cents >= 1)
-	{
-		cents -= 1;
-		coins++;
-	}
-
-	printf("%d\n", coins);
+	printf("%d\n", num_coins);
 
 	return (0);
 }
