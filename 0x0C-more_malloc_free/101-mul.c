5a9fe5d5dd28b4ec0f9f0dbe96c06cbf9c51a221
niyioo <niyiolagbegi@gmail.com>
2023-04-13 11:14:03 -0700

    z

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,52 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - Multiplies two numbers
+ * validate_input - Validates that the input is composed of digits
+ * @input: The input string to be validated
+ *
+ * Return: 0 if the input is composed of digits, 1 otherwise
+ */
+int validate_input(char *input)
+{
+	int i;
+
+	for (i = 0; input[i] != '\0'; i++)
+	{
+		if (!isdigit(input[i]))
+			return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * main - Multiplies two positive numbers
  * @argc: The number of arguments passed
  * @argv: An array containing the arguments passed
- * Return: 0 if successful, 98 if the number of arguments is not 3
- * r if either of the two numbers contains non-digit characters
+ *
+ * Return: 0 if successful, 98 if the number of arguments is not 3,
+ * 1 if either of the two numbers contains non-digit characters
  */
-
 int main(int argc, char *argv[])
 {
 	char *num1_str, *num2_str;
-	int i, num1, num2, result;
+	long int num1, num2, result;
 
+	/* Check for the correct number of arguments */
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
 
+	/* Validate the first argument */
 	num1_str = argv[1];
+	if (validate_input(num1_str))
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	/* Validate the second argument */
 	num2_str = argv[2];
-
-	for (i = 0; num1_str[i] != '\0'; i++)
+	if (validate_input(num2_str))
 	{
-		if (num1_str[i] < '0' || num1_str[i] > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
+		printf("Error\n");
+		return (1);
 	}
 
-	for (i = 0; num2_str[i] != '\0'; i++)
-	{
-		if (num2_str[i] < '0' || num2_str[i] > '9')
-		{
-			printf("Error\n");
-			exit(98);
-		}
-	}
+	/* Convert the arguments to integers */
+	num1 = strtol(num1_str, NULL, 10);
+	num2 = strtol(num2_str, NULL, 10);
 
-	num1 = atoi(num1_str);
-	num2 = atoi(num2_str);
-
+	/* Multiply the two numbers */
 	result = num1 * num2;
 
-	printf("%d\n", result);
+	/* Print the result */
+	printf("%ld\n", result);
 
 	return (0);
 }
