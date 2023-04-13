9a80a628969224007e274c948e323fb71993c478
niyioo <niyiolagbegi@gmail.com>
2023-04-13 11:09:04 -0700

    z

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,122 +1,52 @@
-#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * is_digit - Checks if a character is a digit
- *
- * @c: The character to check
- *
- * Return: 1 if @c is a digit, 0 otherwise
- */
-int is_digit(char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-/**
- * str_len - Computes the length of a string
- *
- * @str: The string to compute the length of
- *
- * Return: The length of @str
- */
-int str_len(char *str)
-{
-	int len = 0;
-
-	while (str[len])
-		len++;
-
-	return (len);
-}
-
-/**
- * str_to_int - Converts a string to an integer
- *
- * @str: The string to convert
- *
- * Return: The integer representation of @str
- *
- * If @str is not a valid integer prints an error message
- * and exits with a status of 98.
+ * main - Multiplies two numbers
+ * @argc: The number of arguments passed
+ * @argv: An array containing the arguments passed
+ * Return: 0 if successful, 98 if the number of arguments is not 3
+ * r if either of the two numbers contains non-digit characters
  */
 
-int str_to_int(char *str)
+int main(int argc, char *argv[])
 {
-	int num = 0;
-	int i;
-
-	for (i = 0; str[i]; i++)
-	{
-		if (!is_digit(str[i]))
-		{
-			_putchar('E');
-			_putchar('r');
-			_putchar('r');
-			_putchar('o');
-			_putchar('r');
-			_putchar('\n');
-			exit(98);
-		}
-
-		num = num * 10 + (str[i] - '0');
-	}
-
-	return (num);
-}
-
-/**
- * print_num - Prints an integer to stdout
- *
- * @num: The integer to print
- *
- * This function recursively prints each digit of @num.
- */
-void print_num(int num)
-{
-	if (num / 10)
-		print_num(num / 10);
-
-	_putchar(num % 10 + '0');
-}
-
-/**
- * main - Entry point for the program
- *
- * @argc: The number of command-line arguments
- * @argv: An array of pointers to the command-line arguments
- *
- * This program multiplies two positive integers passed as
- * command-line arguments and prints the result to stdout
- * If the number of arguments is incorrect or either argument is
- * not a positive integer, prints an error
- * message and exits with a status of 98.
- *
- * Return: 0 on success, 98 on failure
- */
-
-int main(int argc, char **argv)
-{
-	int num1, num2, result;
+	char *num1_str, *num2_str;
+	int i, num1, num2, result;
 
 	if (argc != 3)
 	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
+		printf("Error\n");
 		exit(98);
 	}
 
-	num1 = str_to_int(argv[1]);
-	num2 = str_to_int(argv[2]);
+	num1_str = argv[1];
+	num2_str = argv[2];
+
+	for (i = 0; num1_str[i] != '\0'; i++)
+	{
+		if (num1_str[i] < '0' || num1_str[i] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+
+	for (i = 0; num2_str[i] != '\0'; i++)
+	{
+		if (num2_str[i] < '0' || num2_str[i] > '9')
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
+
+	num1 = atoi(num1_str);
+	num2 = atoi(num2_str);
 
 	result = num1 * num2;
 
-	print_num(result);
-	_putchar('\n');
+	printf("%d\n", result);
 
 	return (0);
 }
