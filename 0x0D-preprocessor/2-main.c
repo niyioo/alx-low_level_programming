41e9225362ea4febe7ebd80ec5cfac8790ccbf5e
niyioo <niyiolagbegi@gmail.com>
2023-04-17 11:02:48 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the file it was
+ * compiled from followed by a new line, using the __FILE__ macro
+ * The program returns 0 to indicate successful completion
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+    printf(__FILE__ "\n");
+    return 0;
+}
