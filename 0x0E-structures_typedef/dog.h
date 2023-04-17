1eff0336ee547543a84b6116dbdf7cd7fa596e33
niyioo <niyiolagbegi@gmail.com>
2023-04-17 12:14:54 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,5 +20,6 @@ typedef struct dog
 
 void init_dog(dog_t *d, char *name, float age, char *owner);
 void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
