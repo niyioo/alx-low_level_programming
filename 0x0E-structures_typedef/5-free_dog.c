34000adb5d94b7e635433c085658f4f2281ddcc7
niyioo <niyiolagbegi@gmail.com>
2023-04-17 12:18:32 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stddef.h>
+
+/**
+ * free_dog - Frees dogs.
+ * @d: Pointer to dog_t struct to free.
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
