d256d338f055ea6cfa0de47e7685b4f53a01cf4a
niyioo <niyiolagbegi@gmail.com>
2023-04-17 11:29:39 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dog's attributes
+ *
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner's name
+ *
+ * Description: This structure defines a dog's attributes
+ * including its name, age, and owner's name.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
