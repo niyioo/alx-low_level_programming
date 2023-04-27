a14d23b02b889aa7b548884329ce0d85cf179b91
niyioo <niyiolagbegi@gmail.com>
2023-04-27 14:44:00 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,15 @@
-section .data
-    ; Define message string and format string
-    message db 'Hello, Holberton', 0
-    format db '%s', 10, 0
+		SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .text
-    global main
-    extern printf
-
+	SECTION .text
+	extern printf
+	global main
 main:
-    ; Push arguments for printf function call onto the stack
-    push format
-    push message
-    ; Call printf function
-    call printf
-    ; Adjust the stack pointer to remove arguments
-    ; Note: we use `sub` instead of `add` because we are subtracting from the stack pointer
-    ; by pushing two values
-    sub rsp, 16
-    ; Return 0 to indicate successful execution
-    mov eax, 0
-    ret
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
+	mov eax, 0
+	ret
