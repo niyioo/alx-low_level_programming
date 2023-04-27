cf85f929605af204b9167498ebbde89289a68118
niyioo <niyiolagbegi@gmail.com>
2023-04-27 12:59:18 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * print_list - prints all the elements of a list_t list
@@ -6,6 +9,7 @@
  *
  * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
