5a5450eb87f1a7910eeb3677e1fa6eaa77fabf36
niyioo <niyiolagbegi@gmail.com>
2023-04-27 13:15:08 -0700

    fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,13 @@
 
 #include <stdio.h>
 
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
