7f6e1a2e0f462778b98c2a1206bd9af7063145cd
niyioo <niyiolagbegi@gmail.com>
2023-05-08 05:29:27 -0700

    fix main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,10 +7,20 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <elf.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char **argv);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_abiversion(unsigned char *e_ident);
+void print_type(Elf64_Half e_type);
+void print_entry(Elf64_Addr e_entry);
 
 #endif
