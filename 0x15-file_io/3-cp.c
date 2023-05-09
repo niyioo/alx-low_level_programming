cc996fdd546c9563cb7a1e15202312cd129d270e
niyioo <niyiolagbegi@gmail.com>
2023-05-09 03:18:44 -0700

    3-cp.c fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,10 +8,10 @@
 #define BUFFER_SIZE 1024
 
 /**
- * Copies the content of file_from to file_to.
+ * copy_file - Copies the content of file_from to file_to.
  *
- * @param file_from the name of the file to read from.
- * @param file_to   the name of the file to write to.
+ * @file_from: the name of the file to read from.
+ * @file_to: the name of the file to write to.
  */
 void copy_file(const char *file_from, const char *file_to)
 {
@@ -20,48 +20,58 @@ void copy_file(const char *file_from, const char *file_to)
 	char buf[BUFFER_SIZE];
 
 	fd_from = open(file_from, O_RDONLY);
-	if (fd_from == -1) {
+	if (fd_from == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1) {
+	if (fd_to == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
-
-	while ((n_read = read(fd_from, buf, BUFFER_SIZE)) > 0) {
-		if (write(fd_to, buf, n_read) != n_read) {
+	while ((n_read = read(fd_from, buf, BUFFER_SIZE)) > 0)
+	{
+		if (write(fd_to, buf, n_read) != n_read)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 			exit(99);
 		}
 	}
-
-	if (n_read == -1) {
+	if (n_read == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-
-	if (close(fd_from) == -1) {
+	if (close(fd_from) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
-	if (close(fd_to) == -1) {
+	if (close(fd_to) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
 }
 
+/**
+ * main - Entry point
+ * @argc: The number of arguments
+ * @argv: The argument vector
+ * Return: Always 0 (success)
+ */
+
 int main(int argc, char *argv[])
 {
-	if (argc != 3) {
+	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		return 97;
+		return (97);
 	}
 
 	copy_file(argv[1], argv[2]);
 
-	return 0;
+	return (0);
 }
