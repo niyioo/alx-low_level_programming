b2a855314c7fbbcdcd8eefd55827606c5c5fada1
niyioo <niyiolagbegi@gmail.com>
2023-07-11 03:08:01 -0700

    fix

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -49,120 +49,33 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	node = ht->array[index];
 
+	node = ht->array[index];
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
-			return (update_value(node, value));
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
 		}
 		node = node->next;
 	}
 
-	new_node = create_new_node(key, value);
-	if (new_node == NULL)
-		return (0);
-
-	insert_node(ht, new_node, index);
-	return (1);
-}
-
-/**
- * update_value - Updates the value of an existing
- * node in the sorted hash table.
- * @node: The node to update.
- * @value: The new value.
- *
- * Return: 1 on success, 0 on failure.
- */
-int update_value(shash_node_t *node, const char *value)
-{
-	char *new_value;
-
-	new_value = strdup(value);
-	if (new_value == NULL)
-		return (0);
-
-	free(node->value);
-	node->value = new_value;
-	return (1);
-}
-
-/**
- * create_new_node - Creates a new node for the sorted hash table.
- * @key: The key.
- * @value: The value associated with the key.
- *
- * Return: A pointer to the new node, or NULL on failure.
- */
-shash_node_t *create_new_node(const char *key, const char *value)
-{
-	shash_node_t *new_node;
-
 	new_node = malloc(sizeof(shash_node_t));
 	if (new_node == NULL)
-		return (NULL);
+		return (0);
 
 	new_node->key = strdup(key);
-	if (new_node->key == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
 	new_node->value = strdup(value);
-	if (new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = NULL;
+	new_node->next = ht->array[index];
 	new_node->sprev = NULL;
 	new_node->snext = NULL;
 
-	return (new_node);
-}
+	if (ht->array[index] != NULL)
+		ht->array[index]->sprev = new_node;
+	ht->array[index] = new_node;
 
-/**
- * insert_node - Inserts a new node into the sorted hash table.
- * @ht: The sorted hash table.
- * @new_node: The new node to insert.
- * @index: The index in the hash table array.
- */
-void insert_node(shash_table_t *ht, shash_node_t *new_node, unsigned long int index)
-{
-	shash_node_t *current, *prev;
-
-	current = ht->array[index];
-	prev = NULL;
-
-	while (current != NULL && strcmp(current->key, new_node->key) < 0)
-	{
-		prev = current;
-		current = current->next;
-	}
-
-	new_node->next = current;
-
-	if (prev == NULL)
-		ht->array[index] = new_node;
-	else
-		prev->next = new_node;
-
-	update_sorted_linked_list(ht, new_node);
-}
-
-/**
- * update_sorted_linked_list - Updates the sorted
- * linked list of the sorted hash table.
- * @ht: The sorted hash table.
- * @new_node: The new node to insert into the sorted linked list.
- */
-void update_sorted_linked_list(shash_table_t *ht, shash_node_t *new_node)
-{
 	if (ht->shead == NULL)
 	{
 		ht->shead = new_node;
@@ -176,22 +89,21 @@ void update_sorted_linked_list(shash_table_t *ht, shash_node_t *new_node)
 	}
 	else
 	{
-		shash_node_t *current = ht->shead;
+		shash_node_t *tmp = ht->shead;
 
-		while (current->snext != NULL &&
-				strcmp(new_node->key, current->snext->key) > 0)
-			current = current->snext;
+		while (tmp->snext != NULL && strcmp(new_node->key, tmp->snext->key) > 0)
+			tmp = tmp->snext;
 
-		new_node->snext = current->snext;
-		new_node->sprev = current;
-
-		if (current->snext != NULL)
-			current->snext->sprev = new_node;
+		new_node->snext = tmp->snext;
+		new_node->sprev = tmp;
+		if (tmp->snext != NULL)
+			tmp->snext->sprev = new_node;
 		else
 			ht->stail = new_node;
-
-		current->snext = new_node;
+		tmp->snext = new_node;
 	}
+
+	return (1);
 }
 
 /**
