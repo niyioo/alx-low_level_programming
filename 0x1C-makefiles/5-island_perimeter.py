b6dd5b0002645f3336aa93e6ac6d991b2ddac84f
niyioo <niyiolagbegi@gmail.com>
2023-08-23 07:09:09 -0700

    fix 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ module to calculate perimeter """
 
+
 def island_perimeter(grid):
     """
     Calculates the perimeter of the island described in grid.
@@ -13,15 +14,14 @@ def island_perimeter(grid):
     """
     rows = len(grid)
     cols = len(grid[0])
-    
+
     perimeter = 0
-    
+
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4  # Every land cell contributes 4 sides to the perimeter
-                
-                # Check adjacent cells and subtract 1 for each adjacent land cell
+                perimeter += 4
+
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 1
                 if i < rows - 1 and grid[i + 1][j] == 1:
@@ -30,6 +30,5 @@ def island_perimeter(grid):
                     perimeter -= 1
                 if j < cols - 1 and grid[i][j + 1] == 1:
                     perimeter -= 1
-    
-    return perimeter
 
+    return perimeter
