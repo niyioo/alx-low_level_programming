b5e58c3b87e19a5b9b17314a1ed6c7dab0525956
niyioo <niyiolagbegi@gmail.com>
2023-10-09 05:09:36 -0700

    s

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stddef.h>
+
+/* Function to perform linear search */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* SEARCH_ALGOS_H */
